e8b44cb786a6b761b02cfd7484cfa989ec3fe370
João Carlos <joaocarlosrm2004@gmail.com>
2024-07-24 13:14:19 -0300

    Ajuste final de personalizacao

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import { livros } from './dadosUltimosLancamentos';
 import { Titulo } from '../Titulo';
+import CardRecomenda from '../CardRecomenda';
+import imagemLivro from '../../imagens/livro2.png';
 
 const UltimosLancamentosContainer = styled.section`
     background-image: linear-gradient(180deg, #000 5%, #EBECEE 30%);
@@ -27,6 +29,12 @@ function UltimoLancamentos(){
                     <img src={livro.src} alt='imagem-lancamento'/>
                 ))}
             </NovosLivrosContainer>
+            <CardRecomenda
+                titulo="Talvez você se interesse por"
+                subtitulo="Angular 11"
+                descricao="Construindo uma aplicação com a plataforma Google"
+                img={imagemLivro}
+            />
         </UltimosLancamentosContainer>
     )
 }
